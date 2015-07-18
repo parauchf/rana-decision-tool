445665bb1e38443655958fdb6a9d2b7139db7e61
Peter Rauchfuss <p.rauchfuss@gmail.com>
2015-07-18 12:37:09 -0400

    first commit

---
+++ index.jsx
@@ -0,0 +1,6 @@
+/** @jsx React.DOM */
+'use strict'
+var React = require('react')
+var Survey = require('./Survey')
+
+React.renderComponent(<Survey />, document.getElementById('content'))
---
+++ survey.jsx
@@ -0,0 +1,9 @@
+'use strict'
+var React = require('react')
+
+module.exports = React.createClass({
+    displayName: 'survey',
+    render: function() {
+        return <div>Survey</div>
+    }
+})
